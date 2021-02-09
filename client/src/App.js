01107b92cb6e860e57875f485e21e7429544b3ec
Ralds Oril <robertjswallow@gmail.com>
2021-02-08 22:57:33 -0800

    added pages and working routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import Flashcards from "./components/Flashcards";
+// import Flashcards from "./components/Flashcards";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import Flashcard from "./pages/flashcardPage"
+import Flashcard from "./pages/flashcardPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Translate from "./pages/Translate"
+import Bootstrap from "bootstrap/dist/css/bootstrap.min.css"
+
+
 
 function App() {
   return   (
     <Router>
       <Route exact path ="/flashcard" component={Flashcard}/>
+      <Route path = "/login" component={Login}/>
+      <Route path = "/register" component={Register} />
+      <Route path = "/translate" component={Translate} />
     </Router>
   )
    
---
+++ client/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React,  {useState} from "react"
+
+function Register() {
+    return(
+<div>
+Register Here
+</div>
+    )
+}
+
+export default Register
