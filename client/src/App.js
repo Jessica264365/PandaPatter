b2acac251ffa409e5b4ae95cd9e46a3e868eead9
Ralds Oril <robertjswallow@gmail.com>
2021-02-08 21:49:53 -0800

    added flahscard component. Added pages folder and flashcardPage.js, created data for cards, an event listener for clicks on card, and a ternery operator to change the display of the cards based on the status

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Flashcards from "./components/Flashcards";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Flashcard from "./pages/flashcardPage"
 
 function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+  return   (
+    <Router>
+      <Route exact path ="/flashcard" component={Flashcard}/>
+    </Router>
+  )
+   
 }
 
 
