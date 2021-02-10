d2ba4a0ae1b568e05ae5bdb7ca9701c8b5af0c6e
mkarkuody <mkarkuody@yahoo.com>
2021-02-09 16:27:14 -0800

    added some lines to app.js and Navbar to test merdad-branch

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,20 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Translate from "./pages/Translate"
 import Bootstrap from "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar"
 
 
 
 function App() {
   return   (
     <Router>
+      <div>
+        <Navbar />
       <Route exact path = "/translate" component={Translate} />
       <Route path ="/flashcard" component={Flashcard}/>
       <Route path = "/login" component={Login}/>
       <Route path = "/register" component={Register} />
-     
+      </div>
     </Router>
   )
    
