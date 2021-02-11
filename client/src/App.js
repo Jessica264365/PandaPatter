e522cd4d04bad7c762b0dbbd3adac4092467bd40
Jessica Whitman <oprah135@gmail.com>
2021-02-10 16:01:57 -0800

    Added UserProvider to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
 import React from "react";
 // import Flashcards from "./components/Flashcards";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Flashcard from "./pages/flashcardPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Translate from "./pages/Translate"
-import Bootstrap from "bootstrap/dist/css/bootstrap.min.css"
-import Navbar from "./components/Navbar"
-
-
+import Translate from "./pages/Translate";
+import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import UserProvider from "./providers/UserProvider";
 
 function App() {
-  return   (
-    <Router>
-      <div>
-        <Navbar />
-      <Route exact path = "/translate" component={Translate} />
-      <Route path ="/flashcard" component={Flashcard}/>
-      <Route exact path = "/" component={Login}/>
-      <Route exact path = "/register" component={Register} />
-      </div>
-    </Router>
-  )
-   
+  return (
+    <UserProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/translate" component={Translate} />
+          <Route path="/flashcard" component={Flashcard} />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+      </Router>
+    </UserProvider>
+  );
 }
-
 
 export default App;
