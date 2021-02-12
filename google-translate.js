1343ee1a685a346832357a31ee9e061639d30257
Bailey Clemons <baileythevampireslayer@gmail.com>
2021-02-11 21:54:33 -0800

    ".env file"

--- google-translate.js
+++ google-translate.js
@@ -1,8 +1,10 @@
-var api = "AIzaSyDW5phlgsH3NLEuMZeJqM6-JXz0KJrpLkY";
+require("dotenv").config();
+
+var api = process.env.API_KEY;
 var googleTranslate = require("google-translate")(api);
 
 var text = "I love cats."
-console.log("English: ",text);
-googleTranslate.translate(text, "ja", function(err, translation) {
-  console.log("Translation: ",translation.translatedText);
+console.log("English: ", text);
+googleTranslate.translate(text, "ja", function (err, translation) {
+  console.log("Translation: ", translation.translatedText);
 });
