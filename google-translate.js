21b02144f4c22f9df381cbcc6c453fb2623360c6
Bailey Clemons <baileymclem@gmail.com>
2021-02-11 21:05:09 -0800

    Delete google-translate.js

--- google-translate.js
+++
@@ -1,8 +0,0 @@
-var api = "AIzaSyDW5phlgsH3NLEuMZeJqM6-JXz0KJrpLkY";
-var googleTranslate = require("google-translate")(api);
-
-var text = "I love cats."
-console.log("English: ",text);
-googleTranslate.translate(text, "ja", function(err, translation) {
-  console.log("Translation: ",translation.translatedText);
-});
