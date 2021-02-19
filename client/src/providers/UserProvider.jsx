1d9814945e2ba7d17e361745b390c04009ef9347
Jessica Whitman <oprah135@gmail.com>
2021-02-18 18:48:15 -0800

    Included generateUserDocument function in Provider to retrieve Firestore data

--- client/src/providers/UserProvider.jsx
+++ client/src/providers/UserProvider.jsx
@@ -1,20 +1,24 @@
 import React, { createContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { auth, generateUserDocument } from "../firebase";
 
 export const UserContext = createContext({ user: null });
+
 function UserProvider(props) {
   const [user, setUser] = useState();
   const [isReady, setisReady] = useState(false);
   useEffect(() => {
     console.log("Waiting for update");
-    auth.onAuthStateChanged((userAuth) => {
+    auth.onAuthStateChanged(async (userAuth) => {
+      const user = await generateUserDocument(userAuth);
       console.log("In UserContext", userAuth);
-      setUser(userAuth);
+      setUser(user);
     });
   }, []);
   useEffect(() => {
     if (typeof user !== "undefined") {
       setisReady(true);
+    } else if (typeof user === "undefined") {
+      setUser(null);
     }
   }, [user]);
   return (
