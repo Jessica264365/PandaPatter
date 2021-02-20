f6cda9a394d4b73322ee25ce29b5868157a15ce1
Jessica Whitman <oprah135@gmail.com>
2021-02-20 12:17:13 -0800

    added uid to schema

--- client/src/pages/Translate.js
+++ client/src/pages/Translate.js
@@ -4,24 +4,24 @@ import React, { useContext, useState } from "react";
 import { UserContext } from "../providers/UserProvider";
 
 function Translate() {
-//   const { user } = useContext(UserContext);
-//   console.log(user.uid);
+  const { user } = useContext(UserContext);
+  console.log(user.uid);
   const [flashcards, setFlashcards] = useState({
     InputLanguage: "",
     OutputLanguage: "",
     Front: "",
     Back: "",
-    // uid: user.uid,
+    uid: user.uid,
   });
 
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     const { name, value } = event.target;
     setFlashcards({
       ...flashcards,
       [name]: value,
     });
   };
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     console.log(flashcards);
     api.saveFlashcards(flashcards).then(function (results) {
       console.log(results);
@@ -292,7 +292,7 @@ function Translate() {
               maxlength="100"
               size="40"
             />
-            <button onChange={handleSubmit} id="go2">
+            <button onClick={handleSubmit} id="go2">
               Save!
             </button>
           </div>
