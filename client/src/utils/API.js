5c80fa83caadb0f0b69f3aa0cf3a3e2ad53780d3
Bailey Clemons <baileymclem@gmail.com>
2021-02-22 19:31:27 -0800

    Delete API.js

--- client/src/utils/API.js
+++
@@ -1,6 +0,0 @@
-// require("dotenv").config();
-//const apiKey = process.env.API_KEY;
-
-const API_KEY='AIzaSyBJMEVDUVn3P5PHmcnBUdrdNOpd4AU0R0o'
-
-export const googleTranslate = require("google-translate")(API_KEY);
